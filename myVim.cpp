5958513579a36e2890c0f254d9564e5f2e892ae5
qiulin <3260296133@qq.com>
2024-04-09 20:26:34 +0800

    实现类的部分方法

--- myVim.cpp
+++ myVim.cpp
@@ -1,1 +1,73 @@
 #include "myVim.h"
+#include<iostream>
+#include <conio.h>
+
+using namespace std;
+
+//插入字符
+void myVim::insert(char ch){
+	lines[currentRow].insert(lines[currentRow].begin() + currentColumn, ch);
+	currentColumn++;
+}
+
+//删除字符
+void myVim::del(string str) {
+	if (currentColumn > 0) {
+		lines[currentRow].erase(lines[currentRow].begin() + currentColumn - 1);
+		currentColumn--;
+	}
+	// TODO: 补充光标在行首时的删除情况
+}
+
+
+
+//光标移动
+void myVim::moveCursor(int x, int y){
+	std::cout << "\033[" << y << ";" << x << "H";
+}
+
+//判断移动方向
+void myVim::judgeMove() {
+	int x = 0;
+	int y = 0;
+	moveCursor(x, y);
+	while (true) {
+		int ch = _getch();
+		if (ch == 224) {
+			ch = _getch();
+			switch (ch) {
+			//向上移动
+			case 72:
+				if (y > 1) {
+					moveCursor(x, --y);
+				}
+				break;
+			//向下移动
+			case 80:
+				moveCursor(x, ++y);
+				break;
+			//向左移动
+			case 75:
+				if (x > 1) {
+					moveCursor(--x, y);
+				}
+					break;
+			case 77:
+				if (x < 100) {
+					moveCursor(++x, y);
+					break;
+				}
+				
+			}
+			
+			
+		}
+	}
+}
+
+void myVim::print() {
+	for (const auto& e : lines) {
+		cout << e << endl;
+	}
+
+}
--- myVim.h
+++ myVim.h
@@ -1,5 +1,23 @@
 #pragma once
-class myVim
-{
+#include<vector>
+#include<string>
+
+
+using namespace std;
+class myVim{
+private:
+	vector<string> lines;
+	size_t currentRow;
+	size_t currentColumn;
+
+public:
+	myVim() :currentRow(0), currentColumn(0) {
+		lines.push_back("");
+	}
+	void insert(char ch);
+	void del(string str);
+	void moveCursor(int x, int y);
+	void judgeMove();
+	void print();
 };
 
